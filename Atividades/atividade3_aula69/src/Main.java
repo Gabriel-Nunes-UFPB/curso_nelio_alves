37b49fdbd4c6351eec15536439f418d98335d62d
gabsi <gabriel-nunes-gomes-2005@outlook.com>
2024-04-25 00:14:28 -0300

    Atividades da aula 69

---
+++ Atividades/atividade3_aula69/src/Main.java
@@ -0,0 +1,22 @@
+import entities.Student;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student student = new Student();
+
+        System.out.println("NAME: ");
+        student.name = sc.nextLine();
+
+        System.out.println("GRADE 1: ");
+        student.grade1 = sc.nextDouble();
+        System.out.println("GRADE 2: ");
+        student.grade2 = sc.nextDouble();
+        System.out.println("GRADE 3: ");
+        student.grade3 = sc.nextDouble();
+
+
+        System.out.println(student.aprovacao());
+
+    }
+}
---
+++ Atividades/atividade3_aula69/src/entities/Student.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Student {
+    public String name;
+    public double grade1, grade2, grade3;
+
+    public double finalGrade(){
+        return (grade1 + grade2 + grade3);
+    }
+
+    public String aprovacao(){
+        if(finalGrade() < 60){
+            return "FAILED\nFINAL GRADE: " + finalGrade() + "\nMISSING " + (60 - finalGrade()) + " POINTS!";
+
+
+        } else {
+            return "PASS\nFINAL GRADE: " + finalGrade();
+        }
+    }
+}
